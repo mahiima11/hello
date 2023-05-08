57ef64c6523bdb14eadf767af8824254f4fa3b67
mahiima11 <ganjimahima@gmail.com>
2023-05-08 18:00:17 +0530

    first commit

---
+++ x/c.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+        printf("\n this is c.c file\n");
+        return 0;
+}
+
---
+++ x/d.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+        printf("\n this is d.c file\n");
+        return 0;
+}
+
