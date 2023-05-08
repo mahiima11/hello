10839f4e6c1a8601267bbbb6844b6c2fd3e1267f
mahiima11 <ganjimahima@gmail.com>
2023-05-08 18:01:01 +0530

    first commit

---
+++ a.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+	printf("\n this is a.c file\n");
+	return 0;
+}
---
+++ b.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+        printf("\n this is b.c file\n");
+        return 0;
+}
+
